ef8870dfee3c0a6b74d949eaa674d43a5c3db1b8
grekinsky <grekinsky@gmail.com>
2020-07-26 19:06:45 -0500

    Refactor Dashboard sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { countFilledCells, countIslands, toggle, resize } from './lib/Grid';
+import { toggle, resize } from './lib/Grid';
 import { initialWidth, initialHeight } from './constants';
 import './App.css';
 import Grid from './components/Grid';
 import Resize from './components/Resize';
+import Dashboard from './components/Dashboard';
 
 function App() {
   const [grid, setGrid] = React.useState(
@@ -26,47 +27,17 @@ function App() {
       </header>
       <section className="App-body">
         <Grid grid={grid} onClickItem={onClickItem} />
-        <aside className="App-panel">
-          <div className="App-panel-size">
-            <section>
-              <strong id="section-width-header">Width: </strong>
-              <span aria-labelledby="section-width-header">
-                {grid[0].length}
-              </span>
-            </section>
-            <section>
-              <strong id="section-height-header">Height: </strong>
-              <span aria-labelledby="section-height-header">{grid.length}</span>
-            </section>
-            <button
-              className="App-button"
-              onClick={() => setResizeModalVisible(true)}
-            >
-              Resize
-            </button>
-          </div>
-          <div className="App-panel-data">
-            <div>
-              <strong id="section-filled-header">Filled: </strong>
-              <span aria-labelledby="section-filled-header">
-                {countFilledCells(grid)}
-              </span>
-            </div>
-            <div>
-              <strong id="section-islands-header">Islands: </strong>
-              <span aria-labelledby="section-islands-header">
-                {countIslands(grid)}
-              </span>
-            </div>
-          </div>
-          <Resize
-            onResize={resizeGrid}
-            hide={() => setResizeModalVisible(false)}
-            isVisible={resizeModalVisible}
-            initialWidth={grid[0].length}
-            initialHeight={grid.length}
-          />
-        </aside>
+        <Dashboard
+          grid={grid}
+          showResizeModal={() => setResizeModalVisible(true)}
+        />
+        <Resize
+          onResize={resizeGrid}
+          hide={() => setResizeModalVisible(false)}
+          isVisible={resizeModalVisible}
+          initialWidth={grid[0].length}
+          initialHeight={grid.length}
+        />
       </section>
     </div>
   );
